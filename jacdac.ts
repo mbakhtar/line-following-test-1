2aec00f7883986cdaa00e673ab51f66392e8a5ba
bilal-inksmith <109742973+mbakhtar@users.noreply.github.com>
2023-02-15 21:47:39 -0500

    Update pxt.json, main.blocks, main.ts, jacdac.json, jacdac.ts

---
+++ jacdac.ts
@@ -0,0 +1,11 @@
+// auto-generated, do not edit.
+namespace myModules {
+
+    //% fixedInstance whenUsed block="reflectedLight2"
+    export const reflectedLight2 = new modules.ReflectedLightClient("reflectedLight2");
+
+    //% fixedInstance whenUsed block="reflectedLight3"
+    export const reflectedLight3 = new modules.ReflectedLightClient("reflectedLight3");
+
+}
+    
--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+basic.showIcon(IconNames.Heart)
+modules.servo1.setEnabled(true)
+modules.servo2.setEnabled(true)
+modules.servo3.setEnabled(true)
 basic.forever(function () {
-	
+    basic.showNumber(myModules.reflectedLight2.brightness())
+    if (myModules.reflectedLight2.brightness() > 50) {
+        modules.servo1.setAngle(0)
+        modules.servo2.setAngle(0)
+        modules.servo3.setAngle(0)
+        basic.pause(1000)
+    }
+    if (myModules.reflectedLight2.brightness() < 50) {
+        modules.servo1.setAngle(90)
+        modules.servo2.setAngle(90)
+        modules.servo3.setAngle(90)
+        basic.pause(1000)
+    }
 })
